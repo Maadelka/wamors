5b2f8c0703f6d05b325274329116b98554fc3f8b
Maadelka <dikarizal62@gmail.com>
2022-04-20 21:22:36 +0700

    fixed bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./public/**/*.{html,js}","./index.html","./src/**/*.{js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'gray-trans':'#373737a9',
+      },
+    },
   },
   plugins: [],
 }
