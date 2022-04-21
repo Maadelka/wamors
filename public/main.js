0239423cc4c9bee4bc81724617f30b2f4121a147
Maadelka <dikarizal62@gmail.com>
2022-04-21 13:36:07 +0700

    update menu setting status

--- public/main.js
+++ public/main.js
@@ -15,8 +15,6 @@ function colseBurger (){
 }
 
 
-
-
 // ------------------------------------------------ icon pindah halaman home,chat,notif,profile
 let home=document.getElementById('home'),
     chat=document.getElementById('chat'),
@@ -26,3 +24,23 @@ let home=document.getElementById('home'),
 notif.addEventListener('click',function(){
     alert('asw')
 })
+
+
+// -------------------- menu setting status
+let tombolMenuSettingStatus=document.getElementById('tombolMenuSettingStatus');
+let menuSettingStatus1=document.getElementById('menuSettingStatus1');
+let tmsi=document.getElementById('tmsi');
+
+
+tombolMenuSettingStatus.addEventListener('click',function(){
+    menuSettingStatus1.classList.toggle('hidden');
+    tmsi.classList.toggle('fa-ellipsis-vertical')
+    tmsi.classList.toggle('fa-xmark')
+})
+
+
+function boolmarkT (e){
+    e.classList.toggle('text-blue-300')
+    e.classList.toggle('text-blue-700')
+    e.classList.toggle('shadow-md')
+}
