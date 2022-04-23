eaa7b9db1afab490ccaba7ef87f0b2ec8bc8315f
Maadelka <dikarizal62@gmail.com>
2022-04-23 14:00:33 +0700

    update warning

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
         'gray-trans':'#373737a9',
       },
     },
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [],
 }
